3b46f51747f2a0c141ce9376f5e790e512eaf843
Stephen Spears <s@spears.biz>
2021-03-11 17:11:48 -0800

    Quotes change

--- helper_fns/select_action_naive.m
+++ helper_fns/select_action_naive.m
@@ -5,7 +5,7 @@ function a = select_action_naive(s, params)
     A = get_action_space(s, image_opps, gstation_opps, N_max, params);
     a = A{1};
     cur_t = s.t;
-    if a.general.type == "image" || a.general.type == "station"
+    if a.general.type == 'image' || a.general.type == 'station'
         s_prime = dynamics_update(s, a, params);
         if (s_prime.p <= params.p_min) || (s_prime.d > params.d_max)
             a = get_nil_action(cur_t);
