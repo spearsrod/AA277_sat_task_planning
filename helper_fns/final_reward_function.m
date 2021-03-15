5ab7962d816c15f330daa5a8338f62dc36ce1ff6
Stephen Spears <s@spears.biz>
2021-03-15 13:52:10 -0700

    Latest reward fn

--- helper_fns/final_reward_function.m
+++ helper_fns/final_reward_function.m
@@ -2,7 +2,8 @@ function [r, cur_I_c, repeat_image] = final_reward_function(s, a, params, combin
 Images = params.Images;
 p_min = params.p_min;
 d_max = params.d_max;
-R_s = s.rewards;
+% R_s = s.rewards;
+R_s = params.rewards;
 gamma = params.gamma;
 comms_reward = params.comms_reward;
 
@@ -19,7 +20,7 @@ if(action_type == "NIL")
     r = 1/(10^4) * (t_s - t);
 elseif(action_type == "image" && (size(combined_I_c, 1) == 0 || not(ismember(a.general.l_geod.', combined_I_c.', 'rows'))))
     [~, index] = ismember(a.general.l_geod.', Images.', 'rows');
-    r = gamma^(t_s - t) * R_s(index);
+    r = R_s(index);%gamma^(t_s - t) * R_s(index);
     cur_I_c = a.general.l_geod;
 elseif(action_type == "image")
     repeat_image = 1;
